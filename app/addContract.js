72e08f488df63266c2f97632ae77531f3182553d
Ahsan Ali Syed <ahsan.syed@codeboxx.biz>
2022-05-26 20:42:04 -0400

    Revert "Ahsan"

--- app/addContract.js
+++ app/addContract.js
@@ -1,37 +1,24 @@
 import {ethers} from 'ethers';
 
-// We get the new provider using ethers...
 const provider = new ethers.providers.Web3Provider(ethereum);
 
-// The params here are what we will pass into index.js when we import it to that file.
 export default async function addContract(id, contract, arbiter, beneficiary, value) {
-  // Set the button id to = interpolated string with id var
   const buttonId = `approve-${id}`;
 
-  // Grab me the container using its id 
   const container = document.getElementById("container");
-  
-  // Lets change the inner html of the container everytime the createHtml method is called and we will pass it the params that we will insert into the input tags.
   container.innerHTML += createHTML(buttonId, arbiter, beneficiary, value);
 
-  // This is a ethers event listener method called .on, it will check to see if the event Approved has happened if yes, it will change some information directly on the DOM
   contract.on('Approved', () => {
-    // On this buttonId, set the class of the button as complete
     document.getElementById(buttonId).className = "complete";
-    // On this buttonId, lets change the text of the button
     document.getElementById(buttonId).innerText = "✓ It's been approved!";
   });
 
-  // Listen for the user to click on button with id ButtonId and run some code
   document.getElementById(buttonId).addEventListener("click", async () => {
-    // Get me the signer from the provider
     const signer = provider.getSigner();
-    // Lets wait for the contract to connect with the signer and for them to approve/sign it.
     await contract.connect(signer).approve();
   });
 }
 
-// This function is responsible for creating the html... We will pass it the necessary params when we use the function above in our index.js
 function createHTML(buttonId, arbiter, beneficiary, value) {
   return `
     <div class="existing-contract">
--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,12 +2,11 @@
 pragma solidity 0.7.5;
 
 contract Escrow {
-	address public depositor; // Signer
-	address payable public beneficiary; // Receiver of funds
-	address public arbiter; // Approver of transaction
+	address public arbiter;
+	address payable public beneficiary;
+	address public depositor;
 
 	bool public isApproved;
-	event Approved(uint256 amountSent);
 
 	constructor(address _arbiter, address payable _beneficiary) payable {
 		arbiter = _arbiter;
@@ -15,16 +14,13 @@ contract Escrow {
 		depositor = msg.sender;
 	}
 
+	event Approved(uint);
+
 	function approve() external {
-		// We want the caller of this function to be our arbiter
 		require(msg.sender == arbiter);
-		// We want the balance to be this smart contracts balance
-		uint256 balance = address(this).balance;
-		// We want to transfer the amount in this contract to the beneficiary
+		uint balance = address(this).balance;
 		beneficiary.transfer(balance);
-		// We want to emit our event so we can listen to it using js
 		emit Approved(balance);
-		// We want let the world know that this contract has been approved
 		isApproved = true;
 	}
 }
